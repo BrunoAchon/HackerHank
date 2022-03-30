b4dc2393c96b421d5db8c72abd80f11bd635902b
unknown <brunoachon@hotmail.com>
2022-03-30 12:29:57 -0300

    HackerRank: minMaxSum

---
+++ minMaxSum/Answer.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    let sum = arr.reduce((a , b) => a + b) // soma toda a lista    
+    let max = Math.max(...arr); // verifica o maior
+    let min = Math.min(...arr); // verifica o menor
+    console.log((sum - max), (sum - min)) // pega a soma e remove o maior e o menor em cada
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
